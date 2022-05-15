325c08460a638aa0912ee0337dcdbdbb81897bdf
Emilio Andrich <eandrichguevara@gmail.com>
2022-05-15 14:29:26 -0400

    Change url settings

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from .views import CategoryView, ProductView
 urlpatterns=[
     path('categories/', CategoryView.as_view(), name='categories_list'),
     path('products/', ProductView.as_view(), name='products_list'),
-    path('products/category/<int:category>/', ProductView.as_view(), name='products_list_by_category'),
+    path('products-by-category/<int:category>/', ProductView.as_view(), name='products_list_by_category'),
     path('products/page/<int:page>/', ProductView.as_view(), name='products_list_by_page'),
 ]
